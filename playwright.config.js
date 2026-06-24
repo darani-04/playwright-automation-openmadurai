06e73282a16252c75970d1dcb3151413e396dc09
Darani A <daraniayyappan0@gmail.com>
2026-06-24 22:06:18 +0530

    update

--- playwright.config.js
+++ playwright.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   reporter: 'html',
 
-   retries: 2,
+  retries: 2,
 
   use: {
     actionTimeout: 30000,
     navigationTimeout: 60000,
+
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure'
   },
 };
